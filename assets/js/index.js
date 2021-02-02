98e0e4868241c4ea62cab0adc4555aba77d38c35
SiobhanBaines <siobhan.baines@gmail.com>
2021-02-02 13:03:40 +0000

    Make crests light up sequentially instead of consecutively

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,15 @@
 /*---------------Global variables-------------*/
-var i = 0;
-var j = 0;
-var nextLevel = false;
-var start = true;
-var delay = 1000;
-var playerArray = [];
-var crestArray = [];
-var winningCrest = "";
+let i = 0;
+let j = 0;
+let l = 0;
+let p = 0;
+let nextLevel = false;
+let start = true;
+let increment = 300;
+let delay = 0;
+let playerArray = [];
+let crestArray = [];
+let winningCrest = "";
 let crests = ['g', 's', 'h', 'r'];
 
 
@@ -30,95 +33,173 @@ function playGame() {
     console.log(crest);
     console.log(crestArray);
 
-    for (i = 0; i < crestArray.length; i++) {
-        console.log(i);
-        lightUp(crest);
+    lightUp();
+
+    playerSelect();
+
+
+
+
+
+};
+/*------function to light up a house crest----------*/
+function lightUp() {
+
+    for (l = 0; l < crestArray.length; l++) {
+        console.log('p', p, 'l', l, 'i', i, 'j', j);
+        crest = crestArray[l];
+        delay = delay + increment;
+        console.log('delay', delay);
+        console.log('inc', increment);
+
+        switch (crest) {
+            case 'g':
+                var element = document.getElementById("gryffindor");
+                    element.classList.add("gryffindor-red");
+                console.log('delay', delay);
+                setTimeout(function () {                
+                    element = document.getElementById("gryffindor");
+                element.classList.remove('gryffindor-red')
+                }, delay);
+                break;
+            case 's':
+                element = document.getElementById("slytherin");
+                element.classList.add("slytherin-green");
+                console.log('delay', delay);     
+                setTimeout(function () {           
+                    element = document.getElementById("slytherin");
+                    element.classList.remove("slytherin-green")
+                }, delay);
+                break;
+            case 'h':
+                element = document.getElementById("hufflepuff");
+                element.classList.add("hufflepuff-yellow");
+                console.log('delay', delay);
+                setTimeout(function () {
+                    element = document.getElementById("hufflepuff");
+                    element.classList.remove("hufflepuff-yellow")
+                }, delay);
+                break;
+            case 'r':
+                element = document.getElementById("ravenclaw");
+                element.classList.add("ravenclaw-blue");
+                console.log('delay', delay);
+                setTimeout(function () {                
+                    element = document.getElementById("ravenclaw");
+                    element.classList.remove("ravenclaw-blue")
+                }, delay);
+                break;
+            default:
+                break;
+        }
     };
 
-
-    $('#gryffindor').click(function gryffindor() {
-        playerArray.push('g');
-        j = playerArray.length - 1;
-        validation();
-    });
-
-    $('#slytherin').click(function slytherin() {
-        playerArray.push('s');
-        j = playerArray.length - 1;
-        validation();
-    });
-
-    $('#hufflepuff').click(function hufflepuff() {
-        playerArray.push('h');
-        j = playerArray.length - 1;
-        validation();
-    });
-
-    $('#ravenclaw').click(function ravenclaw() {
-        playerArray.push('r');
-        j = playerArray.length - 1;
-        validation();
-    });
 };
+function playerSelect() {
 
-/*------function to light up a house crest----------*/
-function lightUp(crest) {
-    switch (crest) {
-        case 'g':
-            var element = document.getElementById("gryffindor");
-            element.classList.add("gryffindor-red")
-            setTimeout(function () {
-                element = document.getElementById("gryffindor");
-                element.classList.remove("gryffindor-red")
-            }, delay);
-            break;
-        case 's':
-            element = document.getElementById("slytherin");
-            element.classList.add("slytherin-green");
-            setTimeout(function () {
-                element = document.getElementById("slytherin");
-                element.classList.remove("slytherin-green")
-            }, delay);
-            break;
-        case 'h':
-            element = document.getElementById("hufflepuff");
-            element.classList.add("hufflepuff-yellow");
-            setTimeout(function () {
-                element = document.getElementById("hufflepuff");
-                element.classList.remove("hufflepuff-yellow")
-            }, delay);
-            break;
-        case 'r':
-            element = document.getElementById("ravenclaw");
-            element.classList.add("ravenclaw-blue");
-            setTimeout(function () {
-                element = document.getElementById("ravenclaw");
-                element.classList.remove("ravenclaw-blue")
-            }, delay);
-            break;
-        default:
-            break;
-    }
+    $('.game-btn').click(function selectHouse() {
+        var house = event.target.id;
+        switch (house) {
+            case 'gryffindor':
+                playerArray.push('g');
+                break;
+            case 'slytherin':
+                playerArray.push('s');
+                break;
+            case 'hufflepuff':
+                playerArray.push('h');
+                break;
+            case 'ravenclaw':
+                playerArray.push('r');
+                break;
+            default:
+                break;
+        };
+        if (crestArray.length === playerArray.length) {
+            j = playerArray.length - 1;
+            validation();
+        };
+    });
+
 };
+/* var myFunction = function () {
+     var att = this.getAttribute()
+ }
+ game_button.addEventListener('click', myFunction(), false);
+
+ /*   for (var p =0; p < crestArray.length; p++){*/
+/* console.log('p', p, 'l', l, 'i', i, 'j', j);
+ $('#gryffindor').click(function gryffindor() {
+     playerArray.push('g');
+     console.log('pA len', playerArray.length);
+     console.log('cA len', crestArray.length);
+     console.log('playerArray:', playerArray);
+     console.log('crestArray:', crestArray);
+     console.log('p', p, 'l', l, 'i', i, 'j', j);
+     if (crestArray.length === playerArray.length) {
+         j = playerArray.length - 1;
+         validation();
+     };
+ });
+
+ $('#slytherin').click(function slytherin() {
+     playerArray.push('s');
+     console.log('pA len', playerArray.length);
+     console.log('cA len', crestArray.length);
+     console.log('p', p, 'l', l, 'i', i, 'j', j);
+     console.log('playerArray:', playerArray);
+     console.log('crestArray:', crestArray);
+     if (crestArray.length === playerArray.length) {
+         j = playerArray.length - 1;
+         validation();
+     };
+ });
+
+ $('#hufflepuff').click(function hufflepuff() {
+     playerArray.push('h');
+     console.log('pA len', playerArray.length);
+     console.log('cA len', crestArray.length);
+     console.log('p', p, 'l', l, 'i', i, 'j', j);
+     console.log('playerArray:', playerArray);
+     console.log('crestArray:', crestArray);
+     if (crestArray.length === playerArray.length) {
+         j = playerArray.length - 1;
+         validation();
+     };
+ });
+
+ $('#ravenclaw').click(function ravenclaw() {
+     playerArray.push('r');
+     console.log('pA len', playerArray.length);
+     console.log('cA len', crestArray.length);
+     console.log('playerArray:', playerArray);
+     console.log('crestArray:', crestArray);
+     console.log('p', p, 'l', l, 'i', i, 'j', j);
+     if (crestArray.length === playerArray.length) {
+         j = playerArray.length - 1;
+         validation();
+     };
+ });
+ /*};*/
+/*};*/
 
 /*------Validate Players Crest Selection -----------*/
 function validation() {
-    console.log(j);
-    console.log(i);
+    console.log('p', p, 'l', l, 'i', i, 'j', j);
     console.log('playerArray[j]:', playerArray[j]);
     console.log('crestArray[j]:', crestArray[j]);
     console.log('playerArray:', playerArray);
     console.log('crestArray:', crestArray);
-    var is_same = (crestArray.length == playerArray.length) && crestArray.every(function(element, index) {
-    return element === playerArray[index]; 
+    var is_same = (crestArray.length == playerArray.length) && crestArray.every(function (element, index) {
+        return element === playerArray[index];
     });
-
+    playerArray = [];
     if (is_same) {
         winnerMessage();
     } else {
         winningCrest = crestArray[j];
         houseMessage();
-    } 
+    }
 };
 
 /*------function to determine which house won-------*/
@@ -141,8 +222,8 @@ function houseMessage() {
         default:
             break;
     };
-playerArray.length=0;
-crestArray.length=0;
+    playerArray.length = 0;
+    crestArray.length = 0;
 };
 
 /*---function to let the player know they have won--*/
@@ -150,7 +231,7 @@ function winnerMessage() {
     $('#gryffindor, #hufflepuff, #slytherin, #ravenclaw, .title').hide();
     $('#dobby').show();
     $('section>div').append(`<p class="win">Dobby thinks you are very clever</p>`);
-    playerArray.length=0;
+    playerArray.length = 0;
 };
 
 /*---------reset to begin the game again ----------*/
@@ -162,7 +243,7 @@ $(document).on('click', '.board', function reStart() {
     nextLevel = false;
     start = true;
 
-   /* playGame();*/
+    /* playGame();*/
 });
 
 /*------play next level of game -------------------*/
